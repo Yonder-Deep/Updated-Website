065e13dc829d6e71e1bea26630d85a601e1de4c9
Brennon <brennonyoo@gmail.com>
2026-08-25 22:53:41 -0700

    Version 1.1

--- yonderdeep-site/src/components/header.jsx
+++ yonderdeep-site/src/components/header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "./Header.css";
+import "./header.css";
 import logo from "../assets/BlackonWhite.inverted.png";
 
 export default function Header() {
